2445eaac7814b057630d525c5256eb92a7559812
Arman.H <ahhdev@gmail.com>
2021-09-30 10:53:57 +0330

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 import config from "./config";
 
 const app = express();
+
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("hello");
