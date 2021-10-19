133645d5e6102a84c480f3ea048f5620adc38b0b
ahhshm <87268103+ahhshm@users.noreply.github.com>
2021-10-19 20:59:56 +0330

    fix typo

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import "express-async-errors";
 import config, { app } from "./config";
 
 app.listen(config.app.port, () => {
-  console.log(`Server is running on port ${config.app.port}`);
+  console.log(`Server is running at port ${config.app.port}`);
 });
